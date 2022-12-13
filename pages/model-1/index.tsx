b144be7173170ecca6b4b279cb9ea6ab2b237976
Renan Marangon <maracunha@gmail.com>
2022-12-13 19:17:08 -0300

    fix: Remove getStaticProps

--- pages/model-1/index.tsx
+++ pages/model-1/index.tsx
@@ -1,17 +1,12 @@
-import { GetServerSideProps } from "next";
 import { useRouter } from 'next/router';
 import Wizard from "../../components/Wizard";
 import StepOne from "./steps/StepOne";
 import StepTwo from "./steps/StepTwo";
 
-type ModelProps = {
-  data: object,
-};
-
-const Model_1 = ({ data }: ModelProps) => {
+const Model_1 = () => {
   const router = useRouter();
-  console.log({ data });
   const initialValues = {};
+
   const onSubmit = () => {
     console.log('Submit')
     router.push('/');
@@ -24,16 +19,5 @@ const Model_1 = ({ data }: ModelProps) => {
       </Wizard>
   );
 };
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log( {context} )
-  const API_URL = 'http://localhost:3000'
-  const res = await fetch(`${API_URL}/api/hello`);
-  const data = await res.json();
-
-  return {
-    props: { data }
-  };
-}
 
 export default Model_1;
